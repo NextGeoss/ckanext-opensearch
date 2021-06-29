3656402e7b823dea5321c7ddb1cdb78e2964ad82
flaviorosadme <82375986+flaviorosadme@users.noreply.github.com>
2021-06-29 18:02:53 +0100

    Update helpers.py

--- ckanext/opensearch/helpers.py
+++ ckanext/opensearch/helpers.py
@@ -107,9 +107,9 @@ def make_entry_resource(resource):
     default = "application/octet-stream"
     mime_type = resource.get("mimetype") or default
     name = resource.get("name", "Untitled")
-    if name.startswith("Metadata Download"):
+    if "Metadata" in name:
         link = {"href": resource["url"], "title": name, "rel": "via", "type": mime_type}
-    elif name.startswith("Thumbnail Download"):
+    elif "Thumbnail" in name:
         link = {
             "href": resource["url"],
             "title": "Quicklook image",
